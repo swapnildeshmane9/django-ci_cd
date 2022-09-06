ef09738dc02f386216355c20f41c4f1e346d4348
swapnil Deshmane <swapnildeshmane9@gmail.com>
2022-09-06 18:59:07 +0530

    File_changes

--- gunicorn.sh
+++ gunicorn.sh
@@ -10,7 +10,7 @@ python3 manage.py collectstatic -- no-input
 
 echo "Migrations done"
 
-cd /var/lib/jenkins/workspace/django-cicd
+cd /var/lib/jenkins/workspace/django-ci_cd
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
